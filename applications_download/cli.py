6d61c94936d98b90fadc75176da8e56a9a651809
Stéphane Brunner <stephane.brunner@camptocamp.com>
2025-02-15 17:03:46 +0100

    Better command structure

--- applications_download/cli.py
+++ applications_download/cli.py
@@ -21,62 +21,59 @@ def main() -> None:
     argparser.add_argument(
         "--versions", help="The file containing the versions of the applications to install"
     )
-    argparser.add_argument("--name", help="The name of the application to install")
-    argparser.add_argument(
+    subparsers = argparser.add_subparsers(dest="cmd")
+
+    subparsers.add_parser("list", help="List the available applications to install")
+
+    install_parser = subparsers.add_parser("install", help="Install applications")
+    install_parser.add_argument("--name", help="The name of the application to install")
+    install_parser.add_argument(
         "--version", help="The version of the application to download, to be used with --name"
     )
-    argparser.add_argument("--list", action="store_true", help="List the available applications to install")
-    argparser.add_argument(
-        "--installed", action="store_true", help="List the installed applications versions"
-    )
-    argparser.add_argument("--uninstall", help="Uninstall the installed application")
-    argparser.add_argument("--update", action="store_true", help="Update the installed applications")
-    argparser.add_argument("--install-all", action="store_true", help="Install all the applications")
+    install_parser.add_argument("--all", action="store_true", help="Install all the applications")
+
+    subparsers.add_parser("installed", help="List the installed applications versions")
+
+    uninstall_parser = subparsers.add_parser("uninstall", help="Uninstall the installed application")
+    uninstall_parser.add_argument("name", help="The name of the application to uninstall")
+
+    subparsers.add_parser("update", help="Update the installed applications")
 
     args = argparser.parse_args()
 
     applications = applications_download.load_applications(args.applications)
-
-    if args.list:
-        versions = applications_download.load_versions(args.versions)
-        for key, app in applications.items():
-            if args.name is None or args.name == key:
-                version = f" ({versions[key]})" if key in versions else ""
-                print(f"{key}{version}: {app['description']}")
-        sys.exit(0)
-
-    if args.installed:
-        installed_applications = applications_download.get_installed_applications()
-        for application, version in installed_applications.items():
-            if args.name is None or args.name == application:
-                print(f"{application}: {version}")
-        sys.exit(0)
-
-    if args.version is not None:
-        assert args.name is not None
-        applications_download.install_all_applications(applications, {args.name: args.version})
-        sys.exit(0)
-
     versions = applications_download.load_versions(args.versions)
 
-    if args.name is not None:
-        applications_download.install_all_applications(applications, {args.name: versions[args.name]})
-        sys.exit(0)
-
-    if args.update:
-        # looks to install all the applications
-        applications_download.update_all_applications(applications, versions)
-        sys.exit(0)
-
-    if args.uninstall is not None:
-        applications_download.uninstall_application(applications, args.uninstall)
-        sys.exit(0)
-
-    if args.install_all:
-        applications_download.install_all_applications(applications, versions)
-        sys.exit(0)
-
-    argparser.print_help()
+    match args.cmd:
+        case "list":
+            for key, app in applications.items():
+                if args.name is None or args.name == key:
+                    version = f" ({versions[key]})" if key in versions else ""
+                    print(f"{key}{version}: {app['description']}")
+        case "install":
+            if args.version is not None:
+                if args.name is None:
+                    sys.stderr.write("The --version argument must be used with the --name argument\n")
+                    sys.exit(1)
+                applications_download.install_all_applications(applications, {args.name: args.version})
+            elif args.name is not None:
+                if args.name not in versions:
+                    sys.stderr.write(f"The version of {args.name} is not defined in the versions file\n")
+                    sys.exit(1)
+                applications_download.install_all_applications(applications, {args.name: versions[args.name]})
+            elif args.all:
+                applications_download.install_all_applications(applications, versions)
+        case "installed":
+            installed_applications = applications_download.get_installed_applications()
+            for application, version in installed_applications.items():
+                if args.name is None or args.name == application:
+                    print(f"{application}: {version}")
+        case "update":
+            applications_download.update_all_applications(applications, versions)
+        case "uninstall":
+            applications_download.uninstall_application(applications, args.name)
+        case _:
+            argparser.print_help()
 
 
 if __name__ == "__main__":
